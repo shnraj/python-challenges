e42db685a6f6d43d9349973b22fc2f55a7518ef4
Sahana Rajasekar <shnraj@gmail.com>
2015-12-18 23:22:16 -0800

    Added is_permutation function and test

--- array_string.py
+++ array_string.py
@@ -1,9 +1,11 @@
 def main():
-    print has_unique_chars('start')
-    print has_unique_chars('asd')
+    print has_unique_chars('start')  # False
+    print has_unique_chars('asd')  # True
 
 
-# Implement an algorithm to determine if a string has all unique characters
+# IMPLEMENT AN ALGORITHM TO DETERMINE IF A STRING HAS ALL UNIQUE CHARACTERS
+
+# My solution - Time: O(n) Space: O(n)
 def has_unique_chars(str):
     character_set = set()
     for character in str:
@@ -12,6 +14,20 @@ def has_unique_chars(str):
         else:
             character_set.add(character)
     return True
+
+# Other solutions: sets and length, in place
+
+
+# DETERMINE IF A STRING IS A PERMUTATION OF ANOTHER
+
+# My solution - Time: O(n) Space: O(2n)
+def is_permutation(str1, str2):
+    str1_set = set(str1)
+    str2_set = set(str2)
+
+    str1_set.symmetric_difference_update(str2_set)
+    return len(str1_set) == 0
+
 
 if __name__ == "__main__":
     # execute only if run as a script
--- array_string_tests.py
+++ array_string_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from array_string import has_unique_chars
+from array_string import is_permutation
 
 
 class TestArrayString(unittest.TestCase):
@@ -9,6 +10,14 @@ class TestArrayString(unittest.TestCase):
         self.assertFalse(has_unique_chars('hip hop'))
         self.assertFalse(has_unique_chars('  '))
         self.assertTrue(has_unique_chars(' '))
+
+    def test_is_permutation(self):
+        self.assertFalse(is_permutation('acb ', 'def'))
+        self.assertTrue(is_permutation('nib', 'bin'))
+        self.assertFalse(is_permutation('nib', 'Nbin'))
+        self.assertFalse(is_permutation('n ', 'n'))
+        self.assertTrue(is_permutation('', ''))
+
 
 if __name__ == '__main__':
     unittest.main()
