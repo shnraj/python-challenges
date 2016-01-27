a96ece620ac14d2e7e3d8a3e2072412e8bc4d844
Sahana Rajasekar <shnraj@gmail.com>
2016-01-27 11:26:28 -0800

    Add queue and tests

--- stack_queue.py
+++ stack_queue.py
@@ -31,3 +31,42 @@ class StackNode():
 
     def get_next_node(self):
         return self.next_node
+
+
+class Queue():
+
+    def __init__(self, head=None):
+        self.head = head
+        self.tail = self.head
+
+    def push(self, data=None):
+        new_node = QueueNode(data=data, next_node=self.head, prev_node=None)
+        if self.head:
+            self.head.prev_node = new_node
+        else:
+            self.tail = new_node
+        self.head = new_node
+
+    def pop(self):
+        if self.tail:
+            data = self.tail.data
+            self.tail.prev_node.next_node = None
+            self.tail = self.tail.prev_node
+            return data
+        return None
+
+    def print_queue(self):
+        current_node = self.head
+        arr = []
+        while current_node:
+            arr.append(current_node.data)
+            current_node = current_node.next_node
+        return arr
+
+
+class QueueNode():
+
+    def __init__(self, data=None, next_node=None, prev_node=None):
+        self.data = data
+        self.next_node = next_node
+        self.prev_node = prev_node
--- stack_queue_tests.py
+++ stack_queue_tests.py
@@ -1,5 +1,6 @@
 import unittest
 from stack_queue import Stack
+from stack_queue import Queue
 
 
 class TestStack(unittest.TestCase):
@@ -15,6 +16,22 @@ class TestStack(unittest.TestCase):
         stack.pop()
         self.assertEqual(stack.print_stack(), [2])
 
+
+class TestQueue(unittest.TestCase):
+
+    def test_push_and_pop(self):
+        queue = Queue()
+        queue.pop()
+        self.assertEqual(queue.print_queue(), [])
+
+        queue.push(data=2)
+        self.assertEqual(queue.print_queue(), [2])
+
+        queue.push(data=4)
+        self.assertEqual(queue.print_queue(), [4, 2])
+
+        queue.pop()
+        self.assertEqual(queue.print_queue(), [4])
 
 if __name__ == '__main__':
     unittest.main()
