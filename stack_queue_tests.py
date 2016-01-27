86e97bfc345d9d8835a4b9a7d0b2221d5d8345fa
Sahana Rajasekar <shnraj@gmail.com>
2016-01-26 22:32:22 -0800

    Add more tests

--- stack_queue_tests.py
+++ stack_queue_tests.py
@@ -9,5 +9,12 @@ class TestStack(unittest.TestCase):
         stack.push(data=2)
         self.assertEqual(stack.print_stack(), [2])
 
+        stack.push(data=4)
+        self.assertEqual(stack.print_stack(), [4, 2])
+
+        stack.pop()
+        self.assertEqual(stack.print_stack(), [2])
+
+
 if __name__ == '__main__':
     unittest.main()
