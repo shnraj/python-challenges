3b69b8d36cbab803c702f48caa03feaf4392cb5d
Sahana Rajasekar <shnraj@gmail.com>
2016-03-15 11:49:08 -0700

    Add tests for concepts

--- concepts.py
+++ concepts.py
@@ -1,17 +1,13 @@
+# Python concept examples
+
+
 # CLOSURES IN PYTHON
 
 # Nested functions that
 # 1) access variables that are local to enclosing scopes,
 # 2) do so when they are executed outside of that scope.
-
-def makeClo(x):
+def makeClosure(x):
     def clo(y):
         # x is "closed" in the definition of clo
         return y + x
-return clo
-
-clo5 = makeClo(5)
-clo10 = makeClo(10)
-
-clo5(5) # returns 10
-clo10(5) # returns 15
+    return clo
