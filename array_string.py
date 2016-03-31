bbd8c8997d7cf9aec3f87b02adfefcac15d43a92
Sahana Rajasekar <shnraj@gmail.com>
2016-03-30 19:48:35 -0700

    Move counter to concepts file

--- array_string.py
+++ array_string.py
@@ -31,34 +31,6 @@ def is_permutation(str1, str2):
 # Other solutions: sorting, hash map lookup
 
 
-# USING COUNTER TO TALLY
-from collections import Counter
-
-
-def count_list():
-    cnt = Counter('sahana')  # Counter({'a': 3, 'h': 1, 's': 1, 'n': 1})
-    cnt2 = Counter(['sahana', 'raj'])  # Counter({'sahana': 1, 'raj': 1})
-
-    # Create a list of elements from the Counter object
-    list(cnt.elements())  # ['a', 'a', 'a', 'h', 's', 'n']
-    list(cnt2.elements())  # ['sahana', 'raj']
-
-    # Counter has 'most_common' that allows you to sort items by their count
-    cnt.most_common(2)  # [('a', 3), ('h', 1)]
-
-    c = Counter(a=3, b=1)
-    d = Counter(a=1, b=2)
-
-    c + d                       # add two counters together:  c[x] + d[x]
-    # Counter({'a': 4, 'b': 3})
-    c - d                       # subtract (keeping only positive counts)
-    # Counter({'a': 2})
-    c & d                       # intersection:  min(c[x], d[x])
-    # Counter({'a': 1, 'b': 1})
-    c | d                       # union:  max(c[x], d[x])
-    # Counter({'a': 3, 'b': 2})
-
-
 # Given string L representing a letter and string N representing a newspaper,
 # return true if the L can be written entirely from N and false otherwise.
 def newspaper_letter(N, L):
--- concepts.py
+++ concepts.py
@@ -34,3 +34,35 @@ cube = partial(power, exponent=3)
 
 # def cube(base):
 #   return power(base, 3)
+
+
+# COUNTER (TO TALLY)
+# Use as a non-unique set
+from collections import Counter
+
+
+def count_list():
+    cnt = Counter('sahana')  # Counter({'a': 3, 'h': 1, 's': 1, 'n': 1})
+    cnt2 = Counter(['sahana', 'raj'])  # Counter({'sahana': 1, 'raj': 1})
+
+    # Create a list of elements from the Counter object
+    list(cnt.elements())  # ['a', 'a', 'a', 'h', 's', 'n']
+    list(cnt2.elements())  # ['sahana', 'raj']
+
+    # Counter has 'most_common' that allows you to sort items by their count
+    cnt.most_common(2)  # [('a', 3), ('h', 1)]
+
+    c = Counter(a=3, b=1)
+    d = Counter(a=1, b=2)
+
+
+    c + d                       # add two counters together:  c[x] + d[x]
+    # Counter({'a': 4, 'b': 3})
+    c - d                       # subtract (keeping only positive counts)
+    # Counter({'a': 2})
+    c & d                       # intersection:  min(c[x], d[x])
+    # Counter({'a': 1, 'b': 1})
+    c | d                       # union:  max(c[x], d[x])
+    # Counter({'a': 3, 'b': 2})
+
+
