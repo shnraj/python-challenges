0c0de2af8526a33d1e0d7b382964795e99f5ca02
Sahana Rajasekar <shnraj@gmail.com>
2016-05-06 00:24:29 -0700

    Add any build in function

--- built_in.py
+++ built_in.py
@@ -1,15 +1,3 @@
-# EXPLORE PYTHON BUILT IN FUNCTIONS
-
-
-def explore():
-    # ABS
-    assert abs(4/3) == 1
-
-    # ALL
-    assert all([1, 2, 3]) is True
-    assert all([1, False, 2]) is False
-    assert all([]) is True
-
 # EXPLORE PYTHON ITERATOR FUNCTIONS
 import itertools
 
@@ -25,3 +13,24 @@ def explore_itertools():
         chained.append(i)
 
     assert chained == [1, 2, 3, 'a', 'b', 'c']
+
+
+# EXPLORE PYTHON BUILT IN FUNCTIONS
+
+
+def explore():
+    # ABS
+    assert abs(4/3) == 1
+
+    # ALL
+    assert all([1, 2, 3]) is True
+    assert all([1, False, 2]) is False
+    assert all([]) is True
+
+    # ANY
+    # return True if all elements of the iterable are true or if the iterable is empty
+    assert any([]) is True
+    assert any((1, 2, 3)) is True
+    assert any({}) is True
+    assert any([0]) is False
+    assert any((False, True)) is False
