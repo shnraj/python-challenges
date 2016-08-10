997d753759581a16f7d454089539b2fd0006c116
Sahana Rajasekar <shnraj@gmail.com>
2016-08-09 18:23:42 -0700

    Add snake

---
+++ snake.py
@@ -0,0 +1,26 @@
+class SnakeGame:
+    def __init__(self):
+        self.snake = new LinkedList((0,0))
+        # board 2D array
+
+    def move(dir):
+        # get new snake head
+
+        # if newHead == food
+        # add head to snake
+        # add to board
+        # new food + update food in board
+
+        # else if newHead out of bounds or snake
+        # end game
+
+        # else
+        # remove tail
+        # add head
+        # update board
+
+
+class LinkedListNode:
+    def __init__(self, head, next=None):
+        self.head = head
+        self.next = next
